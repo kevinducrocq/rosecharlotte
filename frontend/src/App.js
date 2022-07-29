2f7d154e09b65b5ad75754bef46876ac9a54a3cb
kducrocq <kevin.ducrocq@live.fr>
2022-07-29 17:13:45 +0200

    création de l'app

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">rosecharlotte</a>
+      </header>
+      <main>
+        List products
+      </main>
+    </div>
+  );
+}
+
+export default App;
