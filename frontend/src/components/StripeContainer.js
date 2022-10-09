b335c80a911cbdd5ad1f81a30bfbcdd2c973b14f
kducrocq <kevin.ducrocq@live.fr>
2022-10-09 20:48:03 +0200

    keys

--- frontend/src/components/StripeContainer.js
+++ frontend/src/components/StripeContainer.js
@@ -4,7 +4,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import CheckoutForm from './CheckoutForm';
 
 const PUBLIC_KEY =
-  'pk_test_51Lqh9bC3E0acuJ6Jju3YfmPFiSUmjmBsyJAmadK737WFxz8i0ohv84MRXGomknkDoi5uyr4Sygyt5kZEq7CNvTDv00860YzrVi';
+  'pk_live_51Lqh9bC3E0acuJ6J1sLJmV493tvgTMmgqkMcsAYp88JT4LVBiMYjSJGMy5JxvST5NJVpeemoZVVN2WyoJ70m0agx00jvkSY986';
 
 const stripeTestPromise = loadStripe(PUBLIC_KEY);
 
