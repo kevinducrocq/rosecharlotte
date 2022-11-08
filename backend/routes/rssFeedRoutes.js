e8d26f9edfa172109fb51f337400a22762b6d2eb
Aurélien COURTIAL <acourtial@wishibam.com>
2022-11-08 13:38:56 +0100

    fix rss

--- backend/routes/rssFeedRoutes.js
+++ backend/routes/rssFeedRoutes.js
@@ -1,24 +1,40 @@
 import express from 'express';
-import { Product as Item } from '../models/productModel.js';
+import { Product } from '../models/productModel.js';
 import dotenv from 'dotenv';
-import { FeedBuilder, Product, ProductPrice } from 'node-product-catalog-feed';
+import { FeedBuilder, Product as FeedProduct, ProductPrice } from 'node-product-catalog-feed';
 
 dotenv.config();
 
 const rssFeedRouter = express.Router();
 
 rssFeedRouter.get('/products.xml', async (req, res) => {
-  const products = await Item.find();
+  const products = await Product.find();
+
+  const mappedProducts = products.map((product) => {
+    const feedProduct = new FeedProduct();
+    feedProduct.id = product._id;
+    feedProduct.title = product.name;
+    feedProduct.link = process.env.ROOT + 'product/' + product.slug;
+    feedProduct.brand = 'Rose Charlotte & Compagnie';
+    feedProduct.availability = product.countInStock > 0 ? FeedProduct.IN_STOCK : FeedProduct.OUT_OF_STOCK;
+    feedProduct.description = product.description;
+    feedProduct.imageLink = process.env.ROOT + product.image;
+    feedProduct.additionalImageLink = product.images.map((image) => process.env.ROOT + image);
+    feedProduct.price = new ProductPrice(product.price, 'EUR');
+    if(product.soldePrice) {
+      feedProduct.salePrice = new ProductPrice(product.soldePrice, 'EUR');
+    }
+    feedProduct.productType = [product.category, product.subCategory];
+    return feedProduct;
+  })
 
   const xml = new FeedBuilder()
     .withTitle('Produits')
     .withLink(process.env.ROOT)
     .withDescription('Produits de Rose Charlotte & Compagnie');
 
-  products.forEach((product) => {
-    
+  mappedProducts.forEach((product) => {
     xml.withProduct(product);
-    
   });
 
   res.send(xml.buildXml());
