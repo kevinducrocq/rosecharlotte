f214980c204bff3e80a988fcd98c403ed65cafe3
kducrocq <kevin.ducrocq@live.fr>
2022-12-01 14:26:27 +0100

    PUBLIC_KEY

--- frontend/src/components/StripeContainer.js
+++ frontend/src/components/StripeContainer.js
@@ -3,10 +3,10 @@ import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import CheckoutForm from './CheckoutForm';
 
-// const PUBLIC_KEY =
-//   'pk_live_51Lqh9bC3E0acuJ6J1sLJmV493tvgTMmgqkMcsAYp88JT4LVBiMYjSJGMy5JxvST5NJVpeemoZVVN2WyoJ70m0agx00jvkSY986';
 const PUBLIC_KEY =
-  'pk_test_51Lqh9bC3E0acuJ6Jju3YfmPFiSUmjmBsyJAmadK737WFxz8i0ohv84MRXGomknkDoi5uyr4Sygyt5kZEq7CNvTDv00860YzrVi';
+  'pk_live_51Lqh9bC3E0acuJ6J1sLJmV493tvgTMmgqkMcsAYp88JT4LVBiMYjSJGMy5JxvST5NJVpeemoZVVN2WyoJ70m0agx00jvkSY986';
+// const PUBLIC_KEY =
+//   'pk_test_51Lqh9bC3E0acuJ6Jju3YfmPFiSUmjmBsyJAmadK737WFxz8i0ohv84MRXGomknkDoi5uyr4Sygyt5kZEq7CNvTDv00860YzrVi';
 
 const stripeTestPromise = loadStripe(PUBLIC_KEY);
 
